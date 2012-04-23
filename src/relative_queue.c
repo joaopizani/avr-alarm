6daf182a0136e3f4f4f10fcb625604593c23d559
João Paulo Pizani Flor <joaopizani@gmail.com>
2012-04-23 17:49:50 -0300

    Some whitespace deleted, testing the vim-fugitive plugin

--- src/relative_queue.c
+++ src/relative_queue.c
@@ -22,9 +22,7 @@ void relative_queue_insert(event_bin_t* head, event_t e) {
     // first
     if((rank_acc + head->e.rank) > e.rank) { // insert as new head
         new->e.rank = e.rank;
-
         head->e.rank -= new->e.rank;
-
         new->next = head;
         return;
     }
@@ -36,12 +34,9 @@ void relative_queue_insert(event_bin_t* head, event_t e) {
 
         if((rank_acc + succ->e.rank) > e.rank) { // insert here
             new->e.rank = e.rank - rank_acc;
-
             succ->e.rank -= new->e.rank;
-
             pred->next = new;
             new->next = succ;
-
             return;
         }
     }
@@ -49,7 +44,6 @@ void relative_queue_insert(event_bin_t* head, event_t e) {
     // last
     rank_acc += pred->e.rank;
     new->e.rank = e.rank - rank_acc;
-
     pred->next = new;
     new->next = (event_bin_t*) 0;
 }
